7ad143552d491b06a2f21f3a666e803af6e5aa01
NilsBacke <nils.backe@gmail.com>
2017-06-24 02:03:52 -0400

    Too many fragments

---
+++ app/src/main/java/com/example/nils/pilldispenser/Fragments/Saturday/SaturdayAftFragment.java
@@ -0,0 +1,53 @@
+package com.example.nils.pilldispenser.Fragments.Saturday;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.example.nils.pilldispenser.ConfigDayFragmentCustomAdapter;
+import com.example.nils.pilldispenser.Fragments.Saturday.SaturdayActivity;
+import com.example.nils.pilldispenser.ListElement;
+import com.example.nils.pilldispenser.R;
+
+import java.util.ArrayList;
+
+public class SaturdayAftFragment extends Fragment {
+
+    ListView listView;
+    ArrayList<ListElement> list;
+
+    public SaturdayAftFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        // Inflate the layout for this fragment 
+        View view = inflater.inflate(R.layout.fragment_sunday_morn, container, false);
+        listView = (ListView) view.findViewById(R.id.fraglistview);
+        list = new ArrayList<>();
+        list.addAll(((SaturdayActivity)getActivity()).getList("saturdayafternoon"));
+        if (list.isEmpty()) {
+            list.addAll(((SaturdayActivity)getActivity()).getList("pillbay"));
+        }
+        ConfigDayFragmentCustomAdapter adapter = new ConfigDayFragmentCustomAdapter(getActivity(), list, 7, 2);
+        listView.setAdapter(adapter);
+        return view;
+    }
+
+
+}
